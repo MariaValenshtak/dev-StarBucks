0a8855cdc5c444c2864795b4182fffe74979eb42
MariaValenshtak <valenshtakm@gmai.com>
2025-04-28 00:17:45 +0300

    js: forEach

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,9 +1,35 @@
 const burger = document.querySelector('.btn-burger');
 const mobileContainer = document.querySelector('.mobile-container')
 
-burger.addEventListener('click', ()=> {
+function ShowMobileContainer() {
+    mobileContainer.classList.toggle('d-block')
     burger.classList.toggle('active')
-    mobileContainer.classList.toggle('show')
+}
+
+burger.addEventListener('click', ShowMobileContainer)
+
+
+const label = document.querySelectorAll('.label')
+
+label.forEach(item => {
+    item.addEventListener('mouseover', function() {
+        item.classList.add('hovered')
+    })
+
+    item.addEventListener('mouseout', function(){
+        item.classList.remove('hovered')
+    })
 })
- 
+
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach(item => {
+    item.addEventListener('mousedown', function(){
+        item.classList.add('hovered')
+    })
+    item.addEventListener('mouseup', function(){
+        item.classList.remove('hovered')
+    })
+})
+
 
