e02bbe7f7d73c7d764b4a7e05de73a87591e03dd
MariaValenshtak <valenshtakm@gmai.com>
2025-05-22 21:33:52 +0300

    fix: videomodal window

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector('.btn-burger');
 const mobileContainer = document.querySelector('.mobile-container')
 
+
 function ShowMobileContainer() {
     mobileContainer.classList.toggle('d-block')
     burger.classList.toggle('active')
@@ -9,27 +10,27 @@ function ShowMobileContainer() {
 burger.addEventListener('click', ShowMobileContainer)
 
 
-const label = document.querySelectorAll('.label')
+// const label = document.querySelectorAll('.label')
 
-label.forEach(item => {
-    item.addEventListener('mouseover', function() {
-        item.classList.add('hovered')
-    })
+// label.forEach(item => {
+//     item.addEventListener('mouseover', function() {
+//         item.classList.add('hovered')
+//     })
 
-    item.addEventListener('mouseout', function(){
-        item.classList.remove('hovered')
-    })
-})
+//     item.addEventListener('mouseout', function(){
+//         item.classList.remove('hovered')
+//     })
+// })
 
-const btn = document.querySelectorAll('.btn')
+// const btn = document.querySelectorAll('.btn')
 
-btn.forEach(item => {
-    item.addEventListener('mousedown', function(){
-        item.classList.add('hovered')
-    })
-    item.addEventListener('mouseup', function(){
-        item.classList.remove('hovered')
-    })
-})
+// btn.forEach(item => {
+//     item.addEventListener('mousedown', function(){
+//         item.classList.add('hovered')
+//     })
+//     item.addEventListener('mouseup', function(){
+//         item.classList.remove('hovered')
+//     })
+// })
 
 
--- dev/assets/js/modal.js
+++ dev/assets/js/modal.js
@@ -1,19 +1,55 @@
-const triger = document.querySelector('#triger')
-const modal = document.querySelector('.modal')
-const close = document.querySelector('.btn-close')
-const body = document.querySelector('body')
+    // const triger = document.querySelector('#triger')
+// const modal = document.querySelector('.modal')
+// const close = document.querySelector('.btn-close')
+// const body = document.querySelector('body')
 
-function showModal(){
+// function showModal(){
+//     modal.classList.add('d-flex')
+//     body.classList.add('overflow-hidden')
+// }
+
+// function closeModal() {
+//     modal.classList.remove('d-flex')
+//     body.classList.remove('overflow-hidden')
+// }
+
+// triger.addEventListener('click', showModal)
+// close.addEventListener('click', closeModal)
+
+const trigers = document.querySelectorAll('[data-modal]')
+const body = document.querySelector('body')
+const closeTrigers = document.querySelectorAll('[data-close-modal]')
+
+function showModal(modal) {
     modal.classList.add('d-flex')
     body.classList.add('overflow-hidden')
 }
 
-function closeModal() {
-    modal.classList.remove('d-flex')
+function closeModal(event) {
+event.preventDefault()
+console.log(event.target);
+
+if (event.target.hasAttribute('data-close-modal')) {
+    event.target.closest('.modal').classList.remove('d-flex')
     body.classList.remove('overflow-hidden')
+} 
 }
 
-triger.addEventListener('click', showModal)
-close.addEventListener('click', closeModal)
 
+trigers.forEach(
+    function(element) {
+        const modalId = element.getAttribute('data-modal')
+        element.addEventListener('click', function(event){
+            event.preventDefault()
+            const modal = document.getElementById(modalId) 
+            showModal(modal)
+        })
+    }
+)
 
+closeTrigers.forEach(function(element){
+    element.addEventListener('click', function(event){
+        event.preventDefault()
+        closeModal(event)
+    })
+})
