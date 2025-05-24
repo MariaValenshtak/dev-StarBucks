f13d413f15934693fb87742623a00697b97d26f6
MariaValenshtak <valenshtakm@gmai.com>
2025-05-24 21:44:03 +0300

    feat: create product swiper

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,6 +10,39 @@ function ShowMobileContainer() {
 burger.addEventListener('click', ShowMobileContainer)
 
 
+const swiper = new Swiper('.swiper-product', {
+   slidesPerView: 4,
+  loop: true,
+  speed: 400,
+  spaceBetween: 48,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+   breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+
+});
+
 // const label = document.querySelectorAll('.label')
 
 // label.forEach(item => {
